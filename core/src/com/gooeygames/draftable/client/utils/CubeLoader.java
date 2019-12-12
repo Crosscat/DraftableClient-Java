bc621e54c3cbd84de666b7313036a9b52cd3d99d
meowgoocat <meowgoocat@yahoo.com>
2019-12-12 09:19:13 -0500

    Committing some uncommitted stuff

--- core/src/com/gooeygames/draftable/client/utils/CubeLoader.java
+++ core/src/com/gooeygames/draftable/client/utils/CubeLoader.java
@@ -1,8 +1,11 @@
 package com.gooeygames.draftable.client.utils;
 
+import com.gooeygames.draftable.client.cards.Card;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class CubeLoader {
     public static String getCubeDataFromFile(String filepath){
@@ -12,5 +15,17 @@ public class CubeLoader {
             e.printStackTrace();
         }
         return "";
+    }
+
+    public static String getCubeDataFromScryfall(List<String> cardNames){
+        RestClient client = new RestClient();
+        for (String cardName : cardNames){
+            try {
+                client.get("https://api.scryfall.com/cards/named?exact=" + cardName);
+                Card card =
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
